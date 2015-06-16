5bb26e3218f038e5fdb49142aa55d76ecdff4eab
unknown <ugali_guy@hotmail.com>
2015-06-16 14:51:00 -0500

    Initial commit for Programming Project 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  
+  ## makeCacheMatrix takes an input matrix x and returns a list of four functions
+  ## which do the following:
+  ##
+  ## (1) set the value of the matrix
+  ## (2) get the value of the matrix
+  ## (3) set the value of the inverse of x
+  ## (4) get the value of the inverse of x
+  ## 
+  ## This list is input into the function cacheSolve.
+  ## 
+  
+  m <-NULL
+  set <- function(y){
+    # Use the <<- operator to assign a value to an object 
+    # in an environment that is different from the current environment. 
+    #    
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list (set = set,
+        get = get,
+        setsolve = setsolve,
+        getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...){
+  
+  ## Input the list output by makeCacheMatric
+  ## Output the inverse of the matrix that was input into makeCacheMatrix.
+  ## 
+  
+  m <- x$getsolve()
+  
+  # If the inverse has already been calculated, the following conditional
+  # retrieves the cached inverse and bypasses the remaining code.
+  if(!is.null(m)){
+    message("Getting cached matrix")
+    return(m)
+  }
+  
+  # If the above conditional is skipped, we calculate the inverse and also
+  # set the value of the cached inverse via "setsolve".
+  inverse <- x$get()
+  m <- solve(inverse, ...)
+  x$setsolve(m)
+  m
 }
